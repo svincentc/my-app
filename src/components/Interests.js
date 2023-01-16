57e8f1f11d4ea923de6250ff85dbc3893dd1df81
svincentc <68624661+svincentc@users.noreply.github.com>
2023-01-16 04:34:28 -0500

    Fixed body color issue. Needs footer svgs, most other completed other than sizing

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import AboutStyles from './About.module.css'
 
 export default function MyInterests(){
     return(
-        <div>
+        <div className={AboutStyles.aboutMe}>
             <h1> My Interests!</h1>
-            <ul>
+            <ul className={AboutStyles.listItems}>
                 <li>Cooking</li>
                 <li>Coffee</li>
                 <li>Coding!</li>
--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -1,14 +1,21 @@
 import React from 'react'
-
+import Simon from '../guide-simon.jpg'
+import MyInfoCSS from './MyInfo.module.css'
+import LinkedIn from '../linkedIn.svg'
+import Email from '../email.svg'
 
 export default function MyInfo(){
     return(
         <div>
-            
-            <h1>Sean Cerneskie</h1> 
-            <p> Frontend Dev</p>
-            <button>Contact me!</button>
-            <button>LinkedIn</button>
+            <img src={Simon} className={MyInfoCSS.img} alt='Simon the Labradoodle.'></img>
+            <div>
+                <h1 className={MyInfoCSS.whiteTxt}>Sean Cerneskie</h1> 
+                <p className={MyInfoCSS.jobTitle }> Frontend Dev</p>
+            </div>
+            <div className={MyInfoCSS.btnContainer}>
+                <button className={MyInfoCSS.contactMe}><img className={MyInfoCSS.LinkedIn} src={Email} ></img>Email</button>
+                <button className={MyInfoCSS.linkedInBtn}> <img className={MyInfoCSS.LinkedIn} src={LinkedIn} ></img>LinkedIn</button>
+            </div>
         </div>
     )
 }
